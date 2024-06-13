d576712aa635f37a6ec0fdaa01f19ac26eba6540
Ajunjee Selvam <ajunjee.selvam@gmail.com>
2024-06-13 15:10:29 -0400

    Updated dashboard code

--- 3. Dashboard Creation/static/JS/Main.js
+++ 3. Dashboard Creation/static/JS/Main.js
@@ -1,9 +1,9 @@
 // Initialize all the LayerGroups that we'll use. 4 total, 1 each for Stations, Attractions, Schools, and Properties. 
 let layers = {
-  Stations: new L.LayerGroup(),
-  Attractions: new L.LayerGroup(),
-  Schools: new L.LayerGroup(),
-  Properties: new L.LayerGroup(), 
+  // Stations: new L.LayerGroup(),
+  // Attractions: new L.LayerGroup(),
+  // Schools: new L.LayerGroup(),
+  // Properties: new L.LayerGroup(), 
   AllProperties: new L.LayerGroup() 
 };
 
@@ -24,13 +24,13 @@ let baseMaps = {
 // Create the map with our layers.
 let map = L.map("map", {
   //Center around Toronto and set zoom
-  center: [43.0389, -87.9065],
+  center: [43.06, -87.9065],
   zoom: 11.5,
   layers: [
     streetmap,
-    layers.Stations,
-    layers.Attractions,
-    layers.Schools,
+    // layers.Stations,
+    // layers.Attractions,
+    // layers.Schools,
     layers.AllProperties
   ]
 });
@@ -181,103 +181,6 @@ function addAllPropertyMarkers(data) {
   }
 }
 
-// // Create function to fetch school data from the Flask App
-// function fetchSchools() {
-//   // Make a request to the Flask server to get the schools data
-//   fetch('/api/toronto_schools') 
-//     // Parse the response as JSON
-//     .then(response => response.json())
-//     .then(data => {
-//       // Call the function to add markers for the schools data
-//       addSchoolMarkers(data); 
-//     })
-//     // Logs errors, if any
-//     .catch(error => console.error('Error fetching data:', error)); 
-// }
-
-// Create function to add school markers based on the data fetched from the Flask App
-function addSchoolMarkers(response) {
-  // Loop through each item in the JSON data
-  for (let index = 0; index < response.length; index++) {
-    // Assign the item to a variable
-    var school = response[index];
-    // Extract the latitude from the school information and assign it to a variable
-    var lat = school.LAT; 
-    // Extract the longitude from the school information and assign it to a variable
-    var long = school.LONG;
-    // Create a marker based on the latitude and longitude variables, 
-    // Set the icon to the yellow marker created above, and
-    // Add the school information to be shown in the pop-up when a marker is clicked
-    // Add the markers to the Schools layer
-    var schoolMarker = L.marker([lat, long],{icon: yellowIcon}).bindPopup("<b>" + "School: " + "</b>" + school.NAME + "</b>" + "<br>" + "<b>" + "Address: " + "</b>" + school.SOURCE_ADDRESS + "<br>" + "<b>" + "City: " + "</b>" + school.CITY + "</b>" + 
-      "<br>" + "<b>" + "Postal Code: " + "</b>" + school.POSTAL_CODE + "<br>"+"<b>" + "Municipality: "+ "</b>"  + school.MUNICIPALITY ).addTo(layers.Schools); 
-  }
-}
-
-// // Create function to fetch subway stations data from the Flask App
-// function fetchStations() {
-//   // Make a request to the Flask server to get the stations data
-//   fetch('/api/subway_stations') 
-//     // Parse the response as JSON
-//     .then(response => response.json()) 
-//     .then(data => {
-//       // Call the function to add markers for the stations data
-//       addStationMarkers(data);
-//     })
-//     // Logs errors, if any
-//     .catch(error => console.error('Error fetching data:', error)); 
-// }
-
-// Create function to add station markers based on the data fetched from the Flask App
-function addStationMarkers(data) {
-  // Loop through each item in the JSON data
-    for (let i = 0; i < data.length; i++) {
-      // Assign the item to a variable
-      var station = data[i];
-      // Extract the latitude from the station information and assign it to a variable
-      var latitude = station.LAT; 
-      // Extract the longitude from the station information and assign it to a variable
-      var longitude = station.LON; 
-      // Create a marker based on the latitude and longitude variables, 
-      // Set the icon to the green marker created above, and
-      // Add the station information to be shown in the pop-up when a marker is clicked
-      // Add the markers to the Stations layer
-      var marker = L.marker([latitude, longitude],{icon: greenIcon}).addTo(layers.Stations);
-      marker.bindPopup("<b>" + "Station: " + "</b>" + station.Station + "</b>" + "<br>" + "<b>" + "Address: " + "</b>" + station.Address + "<br>" + "<b>" + "City: " + "</b>" + station.City + "</b>" + 
-      "<br>" + "<b>" + "Accessibility: " + "</b>" + station.Accessibility + "<br>" + "<b>" + "Parking Spaces: "+ "</b>" + station.Parking_Spaces + "<br>" + "<b>" + "Average Weekly Ridership: " + "</b>" + station.Ridership_Weekly_Average);
-  };
-}
-
-// // Create function to fetch attractions data from the Flask App
-// function fetchAttractions() {
-//   // Make a request to the Flask server to get the attractions data
-//   fetch('/api/attractions')
-//     // Parse the response as JSON
-//     .then(response => response.json()) 
-//     .then(data => {
-//       // Call the function to add markers for the attractions data
-//       addMarkersToMap(data); 
-//     })
-//     // Logs errors, if any
-//     .catch(error => console.error('Error fetching data:', error)); 
-// }
-
-// Create function to add attractions markers using GeoJSON data
-function addMarkersToMap(data) {
-  L.geoJSON(data,  {
-    onEachFeature: function (feature, layer) {
-       // Extract the coordinates and call the function to add the pop-up information details
-      if (feature.geometry && feature.geometry.coordinates[0]) {
-        // Extract the latitude and longitude from the GeoJSON coordinates
-        const [longitude, latitude] = feature.geometry.coordinates[0];
-        // Bind a popup with the attraction details by calling the function
-        layer.bindPopup(getPopupContent(feature.properties));
-      }
-    }
-  // Add the markers to the Attractions layer
-  }).addTo(layers.Attractions);
-}
-
 // Create a helper function to format the popup content
 function getPopupContent(properties) {
   // Define the layout and information to return
@@ -291,15 +194,15 @@ function getPopupContent(properties) {
 // Create a function to add the 4 layers (Schools, Stations, Attractions, and All Properties) 
 // by calling their respective functions
 map.on('overlayadd', function (eventLayer) {
-  if (eventLayer.name === 'Schools') {
-    fetchSchools();
-  }
-  if (eventLayer.name === 'Stations') {
-    fetchStations();
-  }
-  if (eventLayer.name === 'Attractions') {
-    fetchAttractions();
-  }
+  // if (eventLayer.name === 'Schools') {
+  //   fetchSchools();
+  // }
+  // if (eventLayer.name === 'Stations') {
+  //   fetchStations();
+  // }
+  // if (eventLayer.name === 'Attractions') {
+  //   fetchAttractions();
+  // }
   if (eventLayer.name === 'All Properties') {
     fetchAllProperties();
   }
@@ -333,12 +236,20 @@ function displayDataInTable(data) {
     console.log(data)
       var row = tbody.insertRow();
       var addressCell = row.insertCell(0);
-      var countryCell = row.insertCell(1);
-      var postalcodeCell = row.insertCell(2);
+      var cityCell = row.insertCell(1);
+      var stateCell = row.insertCell(2);
+      var zipcodeCell = row.insertCell(3);
+      var finishedsqftCell = row.insertCell(4);
+      var bdrmsCell = row.insertCell(5);
+      var priceCell = row.insertCell(6);
       // Populate the table with the property information
       addressCell.textContent = property.address;
-      countryCell.textContent = property.city;
-      postalcodeCell.textContent = property.zipcode;
+      cityCell.textContent = property.city;
+      stateCell.textContent = property.state;
+      zipcodeCell.textContent = property.zipcode;
+      finishedsqftCell.textContent = property.finishedsqft;
+      bdrmsCell.textContent = property.bdrms;
+      priceCell.textContent = property.price;
       // Call the functions to display additional information based on the clicked row
       row.addEventListener('click', function() {
           selectRow(this,property);
@@ -401,22 +312,33 @@ function displayPropertyDetails(property) {
   tbody.innerHTML = ''; // Clear previous data
   var row = tbody.insertRow();
   var addressCell = row.insertCell(0);
-  var countryCell = row.insertCell(1);
-  var postalcodeCell = row.insertCell(2);
-  var bedroomCell = row.insertCell(3);
-  var bathfullCell = row.insertCell(4);
-  var bathhalfCell = row.insertCell(5);
-  var cyearCell = row.insertCell(6);
-  var denCell = row.insertCell(7);
+  var cityCell = row.insertCell(1);
+  var stateCell = row.insertCell(2);
+  var zipcodeCell = row.insertCell(3);
+  var styleCell = row.insertCell(4);
+  var extwallCell = row.insertCell(5);
+  var yearbuiltCell = row.insertCell(6);
+  var finishedsqftCell = row.insertCell(7);
+  var lotsizeCell = row.insertCell(8);
+  var bdrmsCell = row.insertCell(9);
+  var bathroomsCell = row.insertCell(10);
+  var priceCell = row.insertCell(11);
+  var predictionCell = row.insertCell(12);
+
   // Populate the table with the property information
   addressCell.textContent = property.address;
-  countryCell.textContent = property.city;
-  postalcodeCell.textContent = property.postal_code;
-  bedroomCell.textContent = property.bedroom;
-  bathfullCell.textContent = property.bathroom_full;
-  bathhalfCell.textContent = property.bathroom_half;
-  cyearCell.textContent = property.construction_year;
-  denCell.textContent = property.den;
+  cityCell.textContent = property.city;
+  stateCell.textContent = property.state;
+  zipcodeCell.textContent = property.zipcode;
+  styleCell.textContent = property.style;
+  extwallCell.textContent = property.extwall;
+  yearbuiltCell.textContent = property.year_built;
+  finishedsqftCell.textContent = property.finishedsqft;
+  lotsizeCell.textContent = property.lotsize;
+  bdrmsCell.textContent = property.bdrms;
+  bathroomsCell.textContent = property.baths_total;
+  priceCell.textContent = property.price;
+  predictionCell.textContent = property.predictions_2025;
 }
 
 // Create a function to get form data from the HTML form element
@@ -433,3 +355,102 @@ document.getElementById("myForm").addEventListener("submit", function (e) {
   e.preventDefault();
   getData(e.target);
 });
+
+
+
+// // Create function to fetch school data from the Flask App
+// function fetchSchools() {
+//   // Make a request to the Flask server to get the schools data
+//   fetch('/api/toronto_schools') 
+//     // Parse the response as JSON
+//     .then(response => response.json())
+//     .then(data => {
+//       // Call the function to add markers for the schools data
+//       addSchoolMarkers(data); 
+//     })
+//     // Logs errors, if any
+//     .catch(error => console.error('Error fetching data:', error)); 
+// }
+
+// // Create function to add school markers based on the data fetched from the Flask App
+// function addSchoolMarkers(response) {
+//   // Loop through each item in the JSON data
+//   for (let index = 0; index < response.length; index++) {
+//     // Assign the item to a variable
+//     var school = response[index];
+//     // Extract the latitude from the school information and assign it to a variable
+//     var lat = school.LAT; 
+//     // Extract the longitude from the school information and assign it to a variable
+//     var long = school.LONG;
+//     // Create a marker based on the latitude and longitude variables, 
+//     // Set the icon to the yellow marker created above, and
+//     // Add the school information to be shown in the pop-up when a marker is clicked
+//     // Add the markers to the Schools layer
+//     var schoolMarker = L.marker([lat, long],{icon: yellowIcon}).bindPopup("<b>" + "School: " + "</b>" + school.NAME + "</b>" + "<br>" + "<b>" + "Address: " + "</b>" + school.SOURCE_ADDRESS + "<br>" + "<b>" + "City: " + "</b>" + school.CITY + "</b>" + 
+//       "<br>" + "<b>" + "Postal Code: " + "</b>" + school.POSTAL_CODE + "<br>"+"<b>" + "Municipality: "+ "</b>"  + school.MUNICIPALITY ).addTo(layers.Schools); 
+//   }
+// }
+
+// // Create function to fetch subway stations data from the Flask App
+// function fetchStations() {
+//   // Make a request to the Flask server to get the stations data
+//   fetch('/api/subway_stations') 
+//     // Parse the response as JSON
+//     .then(response => response.json()) 
+//     .then(data => {
+//       // Call the function to add markers for the stations data
+//       addStationMarkers(data);
+//     })
+//     // Logs errors, if any
+//     .catch(error => console.error('Error fetching data:', error)); 
+// }
+
+// // Create function to add station markers based on the data fetched from the Flask App
+// function addStationMarkers(data) {
+//   // Loop through each item in the JSON data
+//     for (let i = 0; i < data.length; i++) {
+//       // Assign the item to a variable
+//       var station = data[i];
+//       // Extract the latitude from the station information and assign it to a variable
+//       var latitude = station.LAT; 
+//       // Extract the longitude from the station information and assign it to a variable
+//       var longitude = station.LON; 
+//       // Create a marker based on the latitude and longitude variables, 
+//       // Set the icon to the green marker created above, and
+//       // Add the station information to be shown in the pop-up when a marker is clicked
+//       // Add the markers to the Stations layer
+//       var marker = L.marker([latitude, longitude],{icon: greenIcon}).addTo(layers.Stations);
+//       marker.bindPopup("<b>" + "Station: " + "</b>" + station.Station + "</b>" + "<br>" + "<b>" + "Address: " + "</b>" + station.Address + "<br>" + "<b>" + "City: " + "</b>" + station.City + "</b>" + 
+//       "<br>" + "<b>" + "Accessibility: " + "</b>" + station.Accessibility + "<br>" + "<b>" + "Parking Spaces: "+ "</b>" + station.Parking_Spaces + "<br>" + "<b>" + "Average Weekly Ridership: " + "</b>" + station.Ridership_Weekly_Average);
+//   };
+// }
+
+// // Create function to fetch attractions data from the Flask App
+// function fetchAttractions() {
+//   // Make a request to the Flask server to get the attractions data
+//   fetch('/api/attractions')
+//     // Parse the response as JSON
+//     .then(response => response.json()) 
+//     .then(data => {
+//       // Call the function to add markers for the attractions data
+//       addMarkersToMap(data); 
+//     })
+//     // Logs errors, if any
+//     .catch(error => console.error('Error fetching data:', error)); 
+// }
+
+// // Create function to add attractions markers using GeoJSON data
+// function addMarkersToMap(data) {
+//   L.geoJSON(data,  {
+//     onEachFeature: function (feature, layer) {
+//        // Extract the coordinates and call the function to add the pop-up information details
+//       if (feature.geometry && feature.geometry.coordinates[0]) {
+//         // Extract the latitude and longitude from the GeoJSON coordinates
+//         const [longitude, latitude] = feature.geometry.coordinates[0];
+//         // Bind a popup with the attraction details by calling the function
+//         layer.bindPopup(getPopupContent(feature.properties));
+//       }
+//     }
+//   // Add the markers to the Attractions layer
+//   }).addTo(layers.Attractions);
+// }
